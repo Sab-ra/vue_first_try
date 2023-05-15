65f51a0298e9b9af738cfe27a2c706044c985ef9
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-05-15 12:26:06 +0300

    Add more mouse events "@mouse over" "@mouseover" to be precise "@mouseleave" "@dblclick" "@mousemove"

--- src/app/app.js
+++ src/app/app.js
@@ -5,16 +5,27 @@ const app = Vue.createApp({
       showBooks: true,
       title: "Easter on the Hell-Forest Road",
       author: "Keira Moore",
-      age: "44"
+      age: "44",
+      x: 0,
+      y: 0 
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks
+    },
+    handleEvent( e, data ) {
+      console.log( e, e.type )
+      if( data ) {
+        console.log( data )
+      }
+    },
+    handleMousemove( e ) {
+      this.x = e.offsetX
+      this.y = e.offsetY
     }
     
   }
-
 
 }) 
 
