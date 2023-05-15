6e234e8a01ea9c1ab8fa6f46f098b8102025df7a
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-05-15 15:58:35 +0300

    Output lists with v-for

--- src/app/app.js
+++ src/app/app.js
@@ -1,33 +1,21 @@
 const app = Vue.createApp({
-  // template: "<h2>I'm the template</h2>"
   data() {
     return {
       showBooks: true,
-      title: "Easter on the Hell-Forest Road",
-      author: "Keira Moore",
-      age: "44",
-      x: 0,
-      y: 0 
+      books: [
+        { title: 'Easter on the Hell-Forest Road', author: 'Keira Moore' },
+        { title: 'Progratic Programmer', author: 'David Thomas' },
+        { title: 'Ruby for Dummies', author: 'Christopher Haupt' },
+  
+      ],
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks
     },
-    handleEvent( e, data ) {
-      console.log( e, e.type )
-      if( data ) {
-        console.log( data )
-      }
-    },
-    handleMousemove( e ) {
-      this.x = e.offsetX
-      this.y = e.offsetY
-    }
-    
-  }
 
-}) 
+  }
+})
 
 app.mount( '#app' )
- 
