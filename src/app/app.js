1dc52cdf9697cf33d9e58b5455fc3634b2d30cea
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-05-15 17:13:50 +0300

    Apply attribute binding

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: 'http://www.thenetninja.co.uk', 
       showBooks: true,
       books: [
-        { title: 'Easter on the Hell-Forest Road', author: 'Keira Moore' },
-        { title: 'Progratic Programmer', author: 'David Thomas' },
-        { title: 'Ruby for Dummies', author: 'Christopher Haupt' },
+        { title: 'Easter on the Hell-Forest Road', author: 'Keira Moore', img: 'assets/img_1.png' },
+        { title: 'Progratic Programmer', author: 'David Thomas', img: 'assets/img_2.png' },
+        { title: 'Ruby for Dummies', author: 'Christopher Haupt', img: 'assets/img_3.png' },
   
       ],
     }
